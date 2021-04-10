572dfcc16cba5be92d9535d6b2384fd9a7a5f92e
weezee2018 <44323821+weezee2018@users.noreply.github.com>
2021-04-10 12:34:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let result=0
+ 
+for (let timestable = 1; timestable < 13; timestable++)  
+{
+for (let counter = 1; counter < 13; counter++)  
+{     
+result=(counter*timestable);
+console.log(timestable + " x " + counter + " = " + result);
+}
+}
+
+
+
